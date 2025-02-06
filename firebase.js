6db74e4b87c2881fd9d95c25fe71c9920c3e2c12
Sattorov Alixon <uzlegand@mail.ru>
2025-02-07 01:24:59 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZRaqnOUFBxZx_Rlu5912am3i5q5n2LgU",
+    authDomain: "yukbor-5d057.firebaseapp.com",
+    databaseURL: "https://yukbor-5d057-default-rtdb.firebaseio.com",
+    projectId: "yukbor-5d057",
+    storageBucket: "yukbor-5d057.firebasestorage.app",
+    messagingSenderId: "680181273288",
+    appId: "1:680181273288:web:25f6ab13b34ee52cc54bfd",
+    measurementId: "G-CJHJGXQB9B"
+  };
